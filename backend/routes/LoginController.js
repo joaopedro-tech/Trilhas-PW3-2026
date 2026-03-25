c9f15752d718721bfeb6ed112645fa5944be3fd3
joao-tech <joao58.silva@alunos.ifsuldeminas.edu.br>
2026-03-25 12:25:01 -0300

    atualização

---
+++ backend/routes/LoginController.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+const userModel = require("../models/userModel");
+
+const SECRET_KEY = "segredo123";
+
+exports.login = async (req, res) => {
+   const { email, password } = req.body
+   userModel.findByEmail(email, (user) => {
+       if(!user){
+           return res.status(401).json({
+               message: "Usuário não encontrado"
+           })
+       }
+       if(user.password !== password){
+           return res.status(401).json({
+               message: "Senha inválida"
+           })
+       }
+       const token = jwt.sign(
+           {
+               id: user.id,
+               email: user.email,
+               role: user.role
+           },
+           SECRET,
+           { expiresIn: "1h" }
+       )
+
+       res.json({
+           message: "Login realizado com sucesso",
+           token,
+           role: user.role
+       })
+
+   })
+}
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const LoginRoutes = require('./src/routes/LoginRoutes');
+
+// Configurações do servidor
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+app.use('/api/login', LoginRoutes);
+
+// Start o server
+app.listen(PORT, () => {
+  console.log(`Servidor esta rodando na porta ${PORT}`);
+});
+
