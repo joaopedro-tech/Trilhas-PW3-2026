dacfe7175febd60cce84830f1f9158ecd1da114a
joao-tech <joao58.silva@alunos.ifsuldeminas.edu.br>
2026-04-08 12:31:32 -0300

    Backend tela login

---
+++ frontend/assets/js/login.js
@@ -0,0 +1,12 @@
+
+async function login() {
+
+    console.log("Dentro da função login");
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+
+}
